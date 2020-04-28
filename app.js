bb0b51ef565e180ea70b493d293dd13274b7185f
Marc <g40419@gmail.com>
2020-04-28 21:39:34 +0800

    basic setup, top-section complete

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const port = 3000
+const app = express()
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+
+app.set('view engine','handlebars')
+app.use(express.static('public'))
+app.get('/',(req, res)=>{
+  res.render('index')
+})
+
+app.listen(port,()=>{
+  console.log('yoyo now is working')
+})
