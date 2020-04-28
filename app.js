395d4ed0bd2a2cbbf470c5d029bd13405e6ed1ea
Marc <g40419@gmail.com>
2020-04-29 00:19:52 +0800

    Restaurant Listing & Listing UI complete

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const port = 3000
 const app = express()
 const exphbs = require('express-handlebars')
+const list = require('./restaurant.json')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 
 app.set('view engine','handlebars')
 app.use(express.static('public'))
 app.get('/',(req, res)=>{
-  res.render('index')
+  res.render('index',{list : list.results})
 })
 
 app.listen(port,()=>{
